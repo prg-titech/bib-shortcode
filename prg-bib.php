d5f0e7e5acea12a4d8468c7592cf47ca40bed377
Hidehiko Masuhara (linux@laptop) <masuhara@acm.org>
2025-02-01 10:48:40 +0900

    code refactoring: use constants and relative URL

--- prg-bib.php
+++ prg-bib.php
@@ -3,7 +3,7 @@
  * Plugin Name:       PRG Bibliography Shortcode
  * Plugin URI:        https://github.com/prg-titech/bib-shortcode/
  * Description:       Shortcode to embed publication lists.
- * Version:           0.1.14-alpha
+ * Version:           0.1.15-alpha
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Hidehiko Masuhara
@@ -16,6 +16,28 @@
  * Requires Plugins:  
  */
 
+// required JS and CSS file names
+define('PRG_BIB_SCRIPT_FILE', 'resize-iframe.js');
+define('PRG_BIB_STYLE_FILE', 'prg-bib.css');
+// URL to the publications directory where bibtexbrowser PHP code exists
+define('PAPERS_URL', '/papers/');
+// PHP file names for showing the publication list, and the reference list
+define('BIBTEX_BROWSER_URL', PAPERS_URL . 'bibtexbrowser.php');
+define('BIBTEX_INDEX_URL', PAPERS_URL . 'bibindex.php');
+// semicolon separated .bib file names of the publication list,
+// English only and both English and Japanese papers
+define('DOT_BIB_FILES','prg-e.bib;thesis-b.bib;thesis-m.bib;thesis-d.bib');
+define('DOT_BIB_FILES_J', DOT_BIB_FILES . ';prg-j.bib');
+
+// we register and requrie the script and style files under those names
+define('PRG_BIB_SCRIPT', 'resize-iframe-js');
+define('PRG_BIB_STYLE', 'prg-bib-style');
+
+// An identity function for interpolating string with expressions.
+// With this variable, we can write expressions in this way:
+//   global $ev;
+//   ... "   ... {$ev(SOME[$expression])} ...  " ....
+$ev = function($val){return $val;};
 
 /**
  * Our theme/plugin enqueue scripts function.
@@ -24,12 +46,12 @@
 function prg_bib_enqueue_scripts() {
     
     // Register the script in the normal WordPress way.
-    wp_register_script( 'resize-iframe-js',
-                        plugins_url('resize-iframe.js',__FILE__));
+    wp_register_script( PRG_BIB_SCRIPT,
+                        plugins_url(PRG_BIB_SCRIPT_FILE,__FILE__));
 
     // Register the style in the normal WordPress way.
-    wp_register_style( 'prg-bib-style',
-                        plugins_url('prg-bib.css',__FILE__));
+    wp_register_style( PRG_BIB_STYLE,
+                        plugins_url(PRG_BIB_STYLE_FILE,__FILE__));
     
     // Grab the global $post object.
     global $post;
@@ -45,8 +67,8 @@ function prg_bib_enqueue_scripts() {
     // See if the post HAS content and, if so, see if it has our shorcode.
     // if ( isset( $post->post_content ) &&
     //      has_shortcode( $post->post_content, 'prg-bib' ) ) {
-        wp_enqueue_script( 'resize-iframe-js' );
-        wp_enqueue_style( 'prg-bib-style' );
+        wp_enqueue_script( PRG_BIB_SCRIPT );
+        wp_enqueue_style( PRG_BIB_STYLE );
     // }
 }
 add_action( 'wp_enqueue_scripts', 'prg_bib_enqueue_scripts' );
@@ -63,10 +85,10 @@ function prg_bib_show_more( $atts ) {
 
 // list of persons
 function prg_bib_personal_list( $atts, $lang ) {
-    $bib_url = 'https://prg.is.titech.ac.jp/papers/bibtexbrowser.php';
+    $bib_url = BIBTEX_BROWSER_URL;
     $author_param = 'author=' . str_replace(' ','+',$atts['author']);
-    $bib_param_e = 'bib=prg-e.bib;thesis-b.bib;thesis-m.bib;thesis-d.bib';
-    $bib_param_ej = $bib_param_e . ';prg-j.bib';
+    $bib_param_e = 'bib=' . DOT_BIB_FILES;
+    $bib_param_ej = 'bib=' . DOT_BIB_FILES_J;
     if ($lang == "ja") {
         $bib_param_e = $bib_param_ej;
         $ej_button = "";
@@ -75,19 +97,21 @@ function prg_bib_personal_list( $atts, $lang ) {
     <a title='publications both in English and Japanese' 
        href='$bib_url?frameset&amp;$bib_param_ej'>
         <div style='position: absolute; padding-left: 8px; padding-top: 2px;'>
-          <img src='https://prg.is.titech.ac.jp/wp-content/plugins/qtranslate/flags/jp.png' />
+          <img src='/wp-content/plugins/qtranslate/flags/jp.png' />
         </div>
         <div style='position: absolute; padding-left: 2px; padding-top: 10px;'>
-          <img src='https://prg.is.titech.ac.jp/wp-content/plugins/qtranslate/flags/gb.png' />
+          <img src='/wp-content/plugins/qtranslate/flags/gb.png' />
         </div>
         <span class='transparent'>👥</span> 
     </a> 
 HTML;
     }
+
+    global $ev;
     $code = <<<HTML
 <div>
   <div class='linksblock'>
-    <a title='publications of the group' href='https://prg.is.titech.ac.jp/papers/'>👥</a>
+    <a title='publications of the group' href='{$ev(PAPERS_URL)}'>👥</a>
     $ej_button
     <a title='full page view' 
        href='$bib_url?$author_param&amp;$bib_param_e'>
@@ -104,21 +128,24 @@ HTML;
 
 // per article pages
 function prg_bib_articles( $atts ) {
+    global $ev;
     $code = "";
     foreach(preg_split("/\,/", $atts['key']) as $key) {
         $code .= "
 <iframe id='". $key .
-              "' src='https://prg.is.titech.ac.jp/papers/bibtexbrowser.php?key=". $key ."&bib=prg-e.bib;prg-j.bib;thesis-d.bib;thesis-m.bib;thesis-b.bib' class='bibtexbrowser' onload='resizeIframe(this)'></iframe>";
+              "' src='{$ev(BIBTEX_BROWSER_URL)}?key=". $key ."&bib=" .
+              DOT_BIB_FILES_J .
+              "' class='bibtexbrowser' onload='resizeIframe(this)'></iframe>";
     }
     return $code;
 }
 
 // index
 function prg_bib_embed_index( $atts , $no_jump_link, $referer_url ) {
+    global $ev;
     $code = "
-<iframe src='https://prg.is.titech.ac.jp/papers/bibindex.php?keys="
-          . $atts['key'] .
-"&bib=prg-e.bib;prg-j.bib;thesis-d.bib;thesis-m.bib;thesis-b.bib"
+<iframe src='{$ev(BIBTEX_INDEX_URL)}?keys="
+          . $atts['key'] . "&bib={$ev(DOT_BIB_FILES_J)}"
           . ($no_jump_link ? "" : "&parent=" . urlencode($referer_url)) .
           "' class='bibtexbrowser' onload='resizeIframe(this)'></iframe>";
     return $code;
